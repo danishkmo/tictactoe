38eed79284bfe5ed4e445f97c87de8eb1779a932
kaleem <kaleemdinesh.mohammad@cognizant.com>
2024-04-03 19:16:52 -0400

    corrections for input size added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TicTacToeBoard from './components/TicTacToeBoard';
 
@@ -12,6 +12,12 @@ function App() {
   const [isNext, setIsNext] = useState(true);
   const [winnnerStatus, setStatus] = useState<Boolean | string>(false);
   const [moveBy, setMoveBy] = useState('X');
+  const [isDisabled, setDisabled] = useState(false);
+  const [inputSize, setInputSize] = useState(3)
+  const handleInputChange = (value:number) =>{
+    value = value >= 3 && value <= 10 ? value : 3;
+    setInputSize(value);    
+  }
   const calculateWinner = (board: any[]):Boolean | string => {
     const m = board.length;
     const combinations = []
@@ -32,21 +38,42 @@ function App() {
   }
 
   const onSelect = (i: number, j: number) => {
-    let newBoard = [...board]
+    let newBoard = [...board];
+    let winner; 
     newBoard[i] = [...newBoard[i]]; // Make a shallow copy of the row
     newBoard[i][j] = isNext ? 'X' : 'O';
-    setMoveBy(newBoard[i][j]);
     setIsNext(!isNext);
     setBoard(newBoard);
-    setStatus(calculateWinner(newBoard))
+    winner = calculateWinner(newBoard);
+    if(winner)
+    {
+      setMoveBy(newBoard[i][j]);
+      setStatus(true);
+      setDisabled(true);
+    }
   }
+
+  const handleRefresh = ()=> {
+    setBoard(generateBoard(inputSize));
+    setDisabled(false);
+    setStatus(false);
+  }
+
+  useEffect(()=>{
+    setBoard(generateBoard(inputSize))
+  },[inputSize])
 
   return (
     <>
-      <div>
+      <div className='parent-container'>
       <h1>Tic Tac Toe</h1>
-      <TicTacToeBoard board={board} onSelect={onSelect} />
-      {winnnerStatus && <div>{moveBy}, You're a winner</div>}
+      <form>
+        <label>Input Size:</label>
+        <input  style={{marginLeft:'10px'}} value={inputSize} type='number' onChange={e=>handleInputChange(e.target.valueAsNumber)} ></input>
+      </form>
+      <TicTacToeBoard disabled={isDisabled} board={board} onSelect={onSelect} />
+      {winnnerStatus && (<><div>{moveBy}, You're a winner</div>
+      <button onClick={handleRefresh}>Refresh</button></>)}
     </div>
        
     </>
--- src/components/TicTacToeBoard.tsx
+++ src/components/TicTacToeBoard.tsx
@@ -4,13 +4,15 @@ import './TicTacToeBoard.css';
 type BoardProps = {
   board: string[][];
   onSelect: (row: number, col: number) => void;
+  disabled: boolean | undefined
 };
 
-const TicTacToeBoard: React.FC<BoardProps> = ({ board, onSelect }) => {
+
+const TicTacToeBoard: React.FC<BoardProps> = ({ board, onSelect, disabled }) => {
   return (
     <div className="board-container">
       {board.map((row, i) => (
-        <div key={i} className="board-row">
+        <div key={i} style={{pointerEvents: disabled ? "none" : undefined}} className="board-row">
           {row.map((value, j) => (
             <button
               key={j}
